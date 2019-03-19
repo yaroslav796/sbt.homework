04f64bcdf6b7588f20ddf6ada799b0622ca385e5
Yaroslav <yaroslav02@bk.ru>
2019-03-19 21:28:56 +0300

    first commit

---
+++ src/Client.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Client extends Person {
+    String fio;
+    Date birthday;
+    DUL document;
+
+    public Client(String fio, Date birthday, DUL document) {
+        super.name = fio;
+        this.birthday = birthday;
+        this.document = document;
+    }
+}
---
+++ src/ConvertData.java
@@ -0,0 +1,11 @@
+public class ConvertData {
+    Client client;
+    Money money;
+    Currency currency;
+
+    public ConvertData(Client client, Money money, Currency currency) {
+        this.client = client;
+        this.money = money;
+        this.currency = currency;
+    }
+}
